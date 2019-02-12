a247344f8ed5f451e4867ba0d7a754c370c6fd8b
Cody Brewer <cb535114@scc-mmlab-14.local>
2019-02-12 17:46:05 -0500

    Whole Project Upload
    Lerp and Raycasting

---
+++ Week5_InClass_Brewer/Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject bombPrefab;
+
+    //Transform allows us to define the position of the spawn
+    public Transform spawnLocation;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //
+            Instantiate(bombPrefab, spawnLocation.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Week5_InClass_Brewer/Assets/Scripts/UFOMotor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMotor : MonoBehaviour
+{
+
+    Vector3 targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // creates a ray from camera point to terrain/game object
+        if (Input.GetMouseButton(0))
+        {
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // defining the raycast
+            RaycastHit hit;
+            // if the ray cast hits something
+            if (Physics.Raycast(cameraRay, out hit))
+            {
+                //gives the point at which the ray hits
+                //Debug.Log(hit.point);
+                //target position of a new vector3 at the hit point x, y + 5 units, z
+                targetPosition = new Vector3(hit.point.x, hit.point.y + 30, hit.point.z);
+                //transform.position = targetPosition; - makes it warp, we want it to move every frame
+
+            }
+        }
+        //takes the original position and target position and interpolates between them
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 0.05f);
+    }
+}
